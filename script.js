63e4a2c56ef8b521091117b24d858a070a1ed28f
sharadraj97 <sharadraj97@gmail.com>
2024-08-02 00:17:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     let currentSlide = 0;
-    const slides = document.querySelectorAll('.experience-slider .slide');
+    const slides = document.querySelectorAll('.experience-slider .slides');
     const totalSlides = slides.length;
     const prevButton = document.getElementById('prev');
     const nextButton = document.getElementById('next');
