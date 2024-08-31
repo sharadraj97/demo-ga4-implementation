bd263de155958e824b7953e92d45e094911ab243
sharadraj97 <sharadraj97@gmail.com>
2024-08-31 15:20:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-let currentBanner = 0;
-const banners = document.querySelectorAll('.banner');
-
-function nextBanner() {
-    banners[currentBanner].style.display = 'none';
-    currentBanner = (currentBanner + 1) % banners.length;
-    banners[currentBanner].style.display = 'block';
-}
 
 document.addEventListener('DOMContentLoaded', () => {
     banners.forEach((banner, index) => {
